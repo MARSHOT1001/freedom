bd3e984a2aab5f0dc6d782c557267a1f5c8a4df6
marshot <81166581+MARSHOT1001@users.noreply.github.com>
2023-01-27 09:19:38 +0900

    paintFolders

--- js/folder.js
+++ js/folder.js
@@ -9,3 +9,16 @@ let folders = [];
 function saveFoldernames() {
   localStorage.setItem(FOLDERNAME_KEY, JSON.stringify(folders));
 }
+
+function paintFolders(newFolder) {
+  const folder = document.createElement("folder");
+  folder.id = newFolder.id;
+  const span = document.createElement("span");
+  span.innerText = newFolder.text;
+  const button = document.createElement("button");
+  button.innerText = "remove";
+  button.addEventListener("click", deleteFolder);
+  folder.appendChild(span);
+  folder.appendChild(button);
+  folderList.appendChild(folder);
+}
