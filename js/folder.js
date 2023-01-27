f5c72bfd974f4f2e78d48cb7fdf5e4a52191f125
marshot <81166581+MARSHOT1001@users.noreply.github.com>
2023-01-27 12:12:47 +0900

    paintFolders

--- js/folder.js
+++ js/folder.js
@@ -1,6 +1,66 @@
+// const folderForm = document.getElementById("folder-form");
+// const folderInput = document.getElementById("folder-input");
+// const folderList = document.getElementById("folder-list");
+
+// const FOLDERNAME_KEY = "folders";
+
+// let folders = [];
+
+// function saveFoldernames() {
+//   localStorage.setItem(FOLDERNAME_KEY, JSON.stringify(folders));
+// }
+
+// function deleteFolder(event) {
+//   const folder = event.target.parentElement.parentElement;
+//   folderList.removeChild(folder);
+//   folders = folders.filter(
+//     (folderItem) => folderItem.id !== parseInt(folder.id)
+//   );
+//   saveFoldernames();
+// }
+
+// function paintFolders(newFolder) {
+//   const folder = document.createElement("li");
+//   folder.classList.add("folder");
+//   folder.id = newFolder.id;
+//   const title = document.createElement("div");
+//   const span = document.createElement("span");
+//   span.innerText = newFolder.text;
+//   const button = document.createElement("button");
+//   button.innerText = "remove";
+//   button.addEventListener("click", deleteFolder);
+//   title.appendChild(span);
+//   title.appendChild(button);
+//   folder.appendChild(title);
+//   folderList.appendChild(folder);
+// }
+
+// function handleFolderSubmit(event) {
+//   event.preventDefault();
+//   const newFolder = folderInput.value;
+//   folderInput.value = "";
+//   const newFolderObj = {
+//     text: newFolder,
+//     id: Date.now(),
+//   };
+//   folders.push(newFolderObj);
+//   paintFolders(newFolderObj);
+//   saveFoldernames();
+// }
+
+// folderForm.addEventListener("submit", handleFolderSubmit);
+
+// const savedFolders = localStorage.getItem(FOLDERNAME_KEY);
+
+// if (savedFolders !== null) {
+//   const parsedFolders = JSON.parse(savedFolders);
+//   folders = parsedFolders;
+//   parsedFolders.forEach(paintFolders);
+// }
+
 const folderForm = document.getElementById("folder-form");
-const folderInput = folderForm.querySelector("#folder-form input");
-const folderList = document.getElementById("#folder-list");
+const folderInput = document.getElementById("folder-input");
+const folderList = document.getElementById("folder-list");
 
 const FOLDERNAME_KEY = "folders";
 
@@ -10,23 +70,55 @@ function saveFoldernames() {
   localStorage.setItem(FOLDERNAME_KEY, JSON.stringify(folders));
 }
 
-function deleteToDo(event) {
-  const folder = event.target.parentElement;
-  folder.remove();
+function deleteFolder(event) {
+  const folder = event.target.parentElement.parentElement;
+  folderList.removeChild(folder);
   folders = folders.filter(
     (folderItem) => folderItem.id !== parseInt(folder.id)
   );
+  saveFoldernames();
 }
 
-function paintItems(newFolder) {
-  const folder = document.createElement("folder");
+function paintFolders(newFolder) {
+  const folder = document.createElement("li");
+  folder.classList.add("folder");
   folder.id = newFolder.id;
+  const title = document.createElement("div");
   const span = document.createElement("span");
   span.innerText = newFolder.text;
   const button = document.createElement("button");
   button.innerText = "remove";
   button.addEventListener("click", deleteFolder);
-  folder.appendChild(span);
-  folder.appendChild(button);
+  title.appendChild(span);
+  title.appendChild(button);
+  const content = document.createElement("div");
+  content.classList.add("content");
+  content.innerText = newFolder.content;
+  folder.appendChild(title);
+  folder.appendChild(content);
   folderList.appendChild(folder);
+}
+
+function handleFolderSubmit(event) {
+  event.preventDefault();
+  const newFolder = folderInput.value;
+  folderInput.value = "";
+  const newFolderObj = {
+    text: newFolder,
+    content: "",
+    id: Date.now(),
+  };
+  folders.push(newFolderObj);
+  paintFolders(newFolderObj);
+  saveFoldernames();
+}
+
+folderForm.addEventListener("submit", handleFolderSubmit);
+
+const savedFolders = localStorage.getItem(FOLDERNAME_KEY);
+
+if (savedFolders !== null) {
+  const parsedFolders = JSON.parse(savedFolders);
+  folders = parsedFolders;
+  parsedFolders.forEach(paintFolders);
 }
