6dc8eafd1853ed9c483b741872d72a85805da3da
marshot <81166581+MARSHOT1001@users.noreply.github.com>
2023-01-28 10:47:20 +0900

    saveFavs()
    save fav links to localStorage

--- js/folders.js
+++ js/folders.js
@@ -3,6 +3,14 @@ const favInputTitle = document.querySelector(".fav__input-title");
 const favInputUrl = document.querySelector(".fav__input-url");
 const folderContent = document.querySelector(".folder__content");
 
+const FAV_KEY = "favs";
+
+let favs = [];
+
+function saveFavs() {
+  localStorage.setItem(FAV_KEY), JSON.stringify(favs);
+}
+
 function createFav() {
   const fav = document.createElement("div");
   fav.classList.add("fav");
