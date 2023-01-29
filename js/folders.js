6079626e4f28495865441c18f99014a673632e49
marshot <81166581+MARSHOT1001@users.noreply.github.com>
2023-01-29 10:17:07 +0900

    createFav, handleFavSubmit

--- js/folders.js
+++ js/folders.js
@@ -3,15 +3,34 @@ const favInputTitle = document.querySelector(".fav__input-title");
 const favInputUrl = document.querySelector(".fav__input-url");
 const folderContent = document.querySelector(".folder__content");
 
-function createFav() {
-  const fav = document.createElement("div");
-  fav.classList.add("fav");
+favs = [];
+
+function createFav(newFav) {
+  const fav = document.createElement("li");
   const anchor = document.createElement("a");
-  anchor.classList.add("fav__anchor");
+  // innerText에는 favInputTitle로부터 받은 value를
+  anchor.innerText = newFav.text;
+  // href 에는 favInputUrl로부터 받은 value를
+  anchor.href("#");
+  anchor.href = newFav.href;
   const button = document.createElement("i");
   button.classList.add("fas", "fa-times");
   button.addEventListener("click", deleteFav);
   fav.appendChild(anchor);
   fav.appendChild(button);
   folderContent.appendChild(fav);
+}
+
+function handleFavSubmit(event) {
+  event.preventDefault();
+  const newFavTitle = favInputTitle.value;
+  const newFavUrl = favInputUrl.value;
+  favInputTitle.value = "";
+  favInputUrl.value = "";
+  const newFavObj = {
+    text: newFavTitle,
+    href: newFavUrl,
+  };
+  favs.push(newFavObj);
+  createFav(newFavObj);
 }
